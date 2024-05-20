da768109574423d476bdf9c5b693e3f0baae1604
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-05-20 13:44:48 +0530

    weekly-task-2

--- weekly-task-2 c program/Q1.c
+++ weekly-task-2 c program/Q1.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
-int main(){
-int arr[5], sum=0,size;
-printf("Enter the Number :");
-scanf("%d",&arr[0]);
-for (int i = 0; i < size; i++)
-{
-    sum += arr[i];
+arr_sum(int arr[],int len){
+    int sum=0;
+    for (int i = 0; i < len; i++)
+    {
+        /* code */
+        sum+=arr[i];
+    }
+    printf("The sum = %d",sum);
+    
 }
-printf(" Fibonacci of %d is : %d ",arr[0] ,sum);
-    return 0 ;
-}
+main(){
+    int len;
+    printf("Ener the len: ");
+    scanf("%d",&len);
 
+    int arr[len];
+    for (int i = 0; i < len; i++)
+    {
+        printf("arr[%d]: ",i);
+        scanf("%d",&arr[i]);
+    }
+    
+    arr_sum(arr,len);
+}
